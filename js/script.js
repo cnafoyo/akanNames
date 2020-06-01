22a8d370cdb96bcb8cb028268a1ea671f79d378f
Billy Chepkemoi <cnafayo@gmail.com>
2020-06-01 21:16:03 +0300

    male and female arrays

--- js/script.js
+++ js/script.js
@@ -8,6 +8,29 @@
 var dayElement = document.getElementById("day");
 var monthElement = document.getElementById("month");
 var yearElement = document.getElementById("year");
+function showAkanName(day,month,year,gender){
+
+	 var dayOfWeek = Math.trunc(getDayOfWeek(day,month,year));
+
+	 var male = [
+	 {"day":"Sunday","name":"Kwasi"},
+	 {"day":"Monday","name":"Kwadwo"},
+	 {"day":"Tuesday","name":"Kwabena"},
+	 {"day":"Wednesday","name":"Kwaku"},
+	 {"day":"Thursday","name":"Yaw"},
+	 {"day":"Friday","name":"Kofi"},
+	 {"day":"Saturday","name":"Kwame"}
+	 ]
+
+	 var female = [
+	 {"day":"Sunday","name":"Akosua"},
+	 {"day":"Monday","name":"Adwoa"},
+	 {"day":"Tuesday","name":"Abenaa"},
+	 {"day":"Wednesday","name":"Akua"},
+	 {"day":"Thursday","name":"Yaa"},
+	 {"day":"Friday","name":"Afua"},
+	 {"day":"Saturday","name":"Ama"}
+	 ]
 
 }
   function getDayOfWeek(day,month,year){
