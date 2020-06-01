eabf464c1c7683759a4b8ab5e6f9e32d6ada3706
Billy Chepkemoi <cnafayo@gmail.com>
2020-06-01 22:33:54 +0300

    editing and alignment of the pages

--- js/script.js
+++ js/script.js
@@ -1,74 +1,76 @@
-function getFormValues(){
+ function getFormValues(){
 
- var day ="";
- var month = "";
- var year = "";
- var gender = "";
+  var day ="";
+  var month = "";
+  var year = "";
+  var gender = "";
 
 
- var dayElement = document.getElementById("day");
- var monthElement = document.getElementById("month");
- var yearElement = document.getElementById("year");
+  var dayElement = document.getElementById("day");
+  var monthElement = document.getElementById("month");
+  var yearElement = document.getElementById("year");
 
- day = dayElement.options[dayElement.selectedIndex].value;
+  day = dayElement.options[dayElement.selectedIndex].value;
 
- month = monthElement.options[monthElement.selectedIndex].value;
+  month = monthElement.options[monthElement.selectedIndex].value;
 
- year = yearElement.options[yearElement.selectedIndex].value;
+  year = yearElement.options[yearElement.selectedIndex].value;
 
- gender = document.querySelector('input[name=gender]:checked').value;
+  gender = document.querySelector('input[name=gender]:checked').value;
 
- if(gender === ""){
-   alert("You must select gender");
- }else if(parseInt(day)<=0 || parseInt(day) > 31){
-   alert("check if you entered right value for day");
- }else if(parseInt(month)<=0 || parseInt(month) > 12){
-   alert("Check if you eneterd right value for month");
- }else{
-   showAkanName(day,month,year,gender);
- }
+  if(gender === ""){
+    alert("You must select gender");
+  }else if(parseInt(day)<=0 || parseInt(day) > 31){
+    alert("check if you entered right value for day");
+  }else if(parseInt(month)<=0 || parseInt(month) > 12){
+    alert("Check if you eneterd right value for month");
+  }else{
+    showAkanName(day,month,year,gender);
+  }
 
 }
 
 function showAkanName(day,month,year,gender){
 
- var dayOfWeek = Math.trunc(getDayOfWeek(day,month,year));
+  var dayOfWeek = Math.trunc(getDayOfWeek(day,month,year));
 
- var male = [
- {"day":"Sunday","name":"Kwasi"},
- {"day":"Monday","name":"Kwadwo"},
- {"day":"Tuesday","name":"Kwabena"},
- {"day":"Wednesday","name":"Kwaku"},
- {"day":"Thursday","name":"Yaw"},
- {"day":"Friday","name":"Kofi"},
- {"day":"Saturday","name":"Kwame"}
- ]
+  //alert(dayOfWeek);
 
- var female = [
- {"day":"Sunday","name":"Akosua"},
- {"day":"Monday","name":"Adwoa"},
- {"day":"Tuesday","name":"Abenaa"},
- {"day":"Wednesday","name":"Akua"},
- {"day":"Thursday","name":"Yaa"},
- {"day":"Friday","name":"Afua"},
- {"day":"Saturday","name":"Ama"}
- ]
+  var male = [
+  {"day":"Sunday","name":"Kwasi"},
+  {"day":"Monday","name":"Kwadwo"},
+  {"day":"Tuesday","name":"Kwabena"},
+  {"day":"Wednesday","name":"Kwaku"},
+  {"day":"Thursday","name":"Yaw"},
+  {"day":"Friday","name":"Kofi"},
+  {"day":"Saturday","name":"Kwame"}
+  ]
 
- if(gender === "male"){
-   var nameArray = male[dayOfWeek == 0?6:dayOfWeek-1];
-   alert ("You were born on a "+ nameArray.day + " and your Akan name is " + nameArray.name);
- }else{
-   var nameArray = female[dayOfWeek == 0?6:dayOfWeek-1];
-   alert ("You were born on a "+ nameArray.day + " and your Akan name is " + nameArray.name);
- }
+  var female = [
+  {"day":"Sunday","name":"Akosua"},
+  {"day":"Monday","name":"Adwoa"},
+  {"day":"Tuesday","name":"Abenaa"},
+  {"day":"Wednesday","name":"Akua"},
+  {"day":"Thursday","name":"Yaa"},
+  {"day":"Friday","name":"Afua"},
+  {"day":"Saturday","name":"Ama"}
+  ]
+
+  if(gender === "male"){
+    var nameArray = male[dayOfWeek == 0?6:dayOfWeek-1];
+    alert ("You were born on a "+ nameArray.day + " and your Akan name is " + nameArray.name);
+  }else{
+    var nameArray = female[dayOfWeek == 0?6:dayOfWeek-1];
+    alert ("You were born on a "+ nameArray.day + " and your Akan name is " + nameArray.name);
+  }
 }
 
 function getDayOfWeek(day,month,year){
 
- var CC = parseInt(year.substring(0, 2));
- var YY = parseInt(year.substring(2, 4));
- var MM = parseInt(month);
- var DD = parseInt(day);
+  var CC = parseInt(year.substring(0, 2));
+  var YY = parseInt(year.substring(2, 4));
+  var MM = parseInt(month);
+  var DD = parseInt(day);
 
- return (((CC/4)-2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7;
+  return (((CC/4)-2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7;
 }
