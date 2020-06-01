585f1c212db93d1f37c0b98da756502c9bd5f969
Billy Chepkemoi <cnafayo@gmail.com>
2020-06-01 19:09:28 +0300

    add new function getDayOfWeek

--- js/script.js
+++ js/script.js
@@ -3,4 +3,14 @@
   var month = "";
   var year = "";
   var gender = "";
+}
+  function getDayOfWeek(day,month,year){
+
+  var CC = parseInt(year.substring(0, 2));
+  var YY = parseInt(year.substring(2, 4));
+  var MM = parseInt(month);
+  var DD = parseInt(day);
+
+  return (((CC/4)-2*CC-1) + ((5*YY/4)) + ((26*(MM+1)/10)) + DD) % 7;
+
  }
